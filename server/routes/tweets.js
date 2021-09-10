09740e1a1d0c2dfcd2164a9d3e12b12ce043698b
Justin Lam <justin.kh.lam@gmail.com>
2021-09-10 04:43:07 +0000

    initialize project with forked files

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -24,6 +24,7 @@ module.exports = function(DataHelpers) {
     }
 
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+        // ternary - if req.body.user truthy - execute req.body.user : else if falsey - run userHelper.
     const tweet = {
       user: user,
       content: {
