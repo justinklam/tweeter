eba89694466dab0ae33659b502e13cd7bfc9278c
Justin Lam <justin.kh.lam@gmail.com>
2021-09-14 03:44:02 +0000

    change eventlogger for counter to input, space out comments for visibility

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 
-  $('#text-box').on('keyup', function(event) {
+  $('#text-box').on('input', function(event) {
     // stores form input
     let $formInput = $(this);
+
     // targets closest element to the form
     let $forms = $formInput.closest('form');
+
     // targets the counter
     let $textCount = $forms.find('#counter');
+
     // targets the capture length of tweet box
     let textLength = $formInput.val().length;
+
     // character counter
     let charLeft = 140 - textLength;
 
