781bee304e7dad9e375441b036e635058d1b136c
Justin Lam <justin.kh.lam@gmail.com>
2021-09-15 03:42:26 +0000

    add new css styling for other-tweet, implement in index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const timeStamp = timeago.format(1631482946098);
+document.getElementById("other-tweet-footer").innerHTML = timeStamp;
+
+const testTweet = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1631482946098
+};
+
+// $(document).ready(function() {
+//   console.log(`hello-----`)
+//   $.getJSON( "../server/data-files/initial-tweets.json", function( data ) {
+//     // $.getJSON( "./scripts/1.json", function( data ) {
+
+//   console.log('data ------', data)
+//   }
+
+// )});
