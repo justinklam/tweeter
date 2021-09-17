e6b010f7a874c164be85e7b13f06c1cf915a50ef
Justin Lam <justin.kh.lam@gmail.com>
2021-09-17 03:20:45 +0000

    initialize formSubmission.js, add in jQuery/AJAX. add new id to form in index.html

---
+++ public/scripts/formSubmission.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('#submit-form').on('submit', function (event) {
+    event.preventDefault();
+    const data = $( this ).serialize();
+    $.ajax({url: '/tweets', method: 'POST', data: data })
+    .then(function (response) {
+      console.log(`Response: ----`, response)
+      // comment to show its functioning
+    });
+  });
+
+
+});
