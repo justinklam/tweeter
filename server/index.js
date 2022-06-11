d3cce07e7fe526ee2563b4c9b868b63cd5fa57fb
Justin Lam <justin.kh.lam@gmail.com>
2022-06-11 14:51:54 -0400

    code cleanup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Basic express setup:
-
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
@@ -19,7 +17,7 @@ const db = require("./lib/in-memory-db");
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
 // (hint hint).
-//
+
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
@@ -35,5 +33,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Tweeter app is listening on port " + PORT);
+  console.log(`Tweeter app is listening on port ${PORT}! 😼`);
 });
